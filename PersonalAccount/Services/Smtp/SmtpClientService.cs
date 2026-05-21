c73fb48bd8e3d00c16115ef04341e752ad1f322e
Alexandr Shamraev <alexandr.shamraev.github@gmail.com>
2026-05-21 16:44:59 +0300

    Добавлен сервис, реализующий логику Smtp-клиента

---
+++ PersonalAccount/Services/Smtp/SmtpClientService.cs
@@ -0,0 +1,39 @@
+﻿using MailKit.Net.Smtp;
+using MailKit.Security;
+using Microsoft.Extensions.Options;
+using MimeKit;
+
+namespace PersonalAccount.Services.Smtp;
+
+public class SmtpClientService(IOptions<SmtpSettings> options) : ISmtpClientService
+{
+    private readonly SmtpSettings _settings = options.Value;
+
+    public async Task SendEmailAsync(string to, string subject, string body) =>
+        await SendMessageAsync(CreateEmailMessage(to, subject, body));
+
+    private async Task SendMessageAsync(MimeMessage message)
+    {
+        using var client = new SmtpClient();
+        client.Timeout = _settings.Timeout;
+
+        await client.ConnectAsync(_settings.Host, _settings.Port, SecureSocketOptions.StartTls);
+        await client.AuthenticateAsync(_settings.Username, _settings.Password);
+        await client.SendAsync(message);
+        await client.DisconnectAsync(true);
+    }
+
+    private MimeMessage CreateEmailMessage(string to, string subject, string body)
+    {
+        var message = new MimeMessage();
+        message.From.Add(new MailboxAddress(_settings.FromName, _settings.FromEmail));
+        message.To.Add(MailboxAddress.Parse(to));
+        message.Subject = subject;
+        message.Body = new BodyBuilder
+        {
+            HtmlBody = body,
+        }.ToMessageBody();
+
+        return message;
+    }
+}
